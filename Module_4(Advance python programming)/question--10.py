d78bdda25c1749cec0d433bbcaa711d717d07953
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 13:50:55 +0530

    Update question--10.py

--- Module_4(Advance python programming)/question--10.py
+++ Module_4(Advance python programming)/question--10.py
@@ -1,15 +1,14 @@
 # Write a Python program to write a list to a file. 
 
-def write_list_to_file(file_path, my_list):
+def write_list_to_file(file_path, my_list):  # Initialize the write list to file
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w') as file:  # open the file
             for item in my_list:
-                file.write(str(item) + '\n')
-        print("List has been written to the file successfully!")
+                file.write(str(item) + '\n')  # write a string in new line
+        print("List has been written to the file successfully!")  # print the list 
     except Exception as e:
-        print("An error occurred:", e)
+        print("An error occurred:", e) # if the error occurs
 
-# Example usage:
-file_path = "output.txt"  # Specify the path to the file where you want to write the list
-my_list = [1, 2, 3, 4, 5]  # Replace this with the list you want to write
-write_list_to_file(file_path, my_list)
+file_path = "output.txt"  # Specify the path of the file to write the list
+my_list = [1, 2, 3, 4, 5]  # list
+write_list_to_file(file_path, my_list)  # write list to file
