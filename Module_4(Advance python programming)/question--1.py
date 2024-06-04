b5e9b866eb571e251f66d0e0efeb50fd58dc0779
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 13:47:43 +0530

    Update question--1.py

--- Module_4(Advance python programming)/question--1.py
+++ Module_4(Advance python programming)/question--1.py
@@ -1,12 +1,12 @@
 # Write a Python program to read an entire text file. 
 
-def read_file(file_path):
+def read_file(file_path): # Initialize the file to read
     try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print("File not found!")
+        with open(file_path, 'r') as file: # open the file
+            content = file.read()  # read the content
+            print(content) # print the content
+    except FileNotFoundError:  
+        print("File not found!")  # print the error
 
-file_path = "sample.txt" 
-read_file(file_path)
+file_path = "sample.txt" # file path
+read_file(file_path)  # read the file 
