5bf30b3f1b2aa8af11d177fcfd7642fea50cfcaf
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 14:02:17 +0530

    Update question--14.py

--- Module_4(Advance python programming)/question--14.py
+++ Module_4(Advance python programming)/question--14.py
@@ -1,22 +1,21 @@
 # Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle 
 
-import math
+import math  # import math module
 
-class Circle:
-    def __init__(self, radius):
+class Circle:  # make a class for circle
+    def __init__(self, radius):  
         self.radius = radius
 
-    def compute_area(self):
+    def calculate_area(self):
         return math.pi * self.radius ** 2
 
-    def compute_perimeter(self):
+    def calculate_perimeter(self):
         return 2 * math.pi * self.radius
 
-# Example usage:
 # Create a circle with radius 5
 circle = Circle(5)
-# Compute the area and perimeter of the circle
-area = circle.compute_area()
-perimeter = circle.compute_perimeter()
-print("Area of the circle:", area)
-print("Perimeter of the circle:", perimeter)
+# Calculate the area and perimeter of the circle
+area = circle.calculate_area()  # calculate the area of circle
+perimeter = circle.calculate_perimeter()  # calculate the perimeter of circle
+print("Area of the circle:", area)  # print the area of the circle
+print("Perimeter of the circle:", perimeter)  # Print the perimeter of the circle
