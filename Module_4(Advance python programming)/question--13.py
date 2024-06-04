20eb354ab2519b2d21a031d015fc6270a0bcf83a
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 13:59:40 +0530

    Update question--13.py

--- Module_4(Advance python programming)/question--13.py
+++ Module_4(Advance python programming)/question--13.py
@@ -1,16 +1,15 @@
 # Write a Python class named Rectangle constructed by a length and width and a method which will compute the area of a rectangle
 
-class Rectangle:
-    def __init__(self, length, width):
+class Rectangle:  # build a class for rectangle
+    def __init__(self, length, width):  # get the length and width
         self.length = length
         self.width = width
 
-    def compute_area(self):
+    def calculate_area(self):  # get the area of rectangle
         return self.length * self.width
 
-# Example usage:
 # Create a rectangle with length 5 and width 3
 rectangle = Rectangle(5, 3)
-# Compute the area of the rectangle
-area = rectangle.compute_area()
-print("Area of the rectangle:", area)
+# Calculate the area of the rectangle
+area = rectangle.calculate_area()   # formula of area of rectangle
+print("Area of the rectangle:", area)  # print the area of rectangle
