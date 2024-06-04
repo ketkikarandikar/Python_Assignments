3d79a1f04c44c3b4f5e108f40294f508bb48ea5d
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 13:37:59 +0530

    Delete Module_4(Advance python programming) directory

--- Module_4(Advance python programming)/question--1.py
+++
@@ -1,6 +0,0 @@
-# What is File function in python? What is keywords to create and write file. 
-
-# File function is used in Python to do file handling.
-# To create a file in Python, you can use the open() function with the 'a' mode such as file = open('myfile. txt', 'a') . 
-# This function opens a file for writing, creating the file if it does not exist. 
-# In this example, we're using the open() function to create a file named 'myfile.
