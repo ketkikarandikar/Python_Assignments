8821317774dfc8cb83a4d72e9b8897dc50458e83
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 13:57:02 +0530

    Update question--12.py

--- Module_4(Advance python programming)/question--12.py
+++ Module_4(Advance python programming)/question--12.py
@@ -1,6 +1,6 @@
 # Write python program that user to enter only odd numbers, else will raise an exception. 
 
-class EvenNumberError(Exception):
+class EvenNumberError(Exception):  # make a class for even number error
     """Custom exception class for handling even number input."""
 
     def __init__(self, value):
@@ -10,19 +10,17 @@ class EvenNumberError(Exception):
         return f"Error: {self.value} is an even number. Please enter only odd numbers."
 
 
-def get_odd_number_from_user():
+def get_odd_number_from_user(): # definition for odd number
     while True:
         try:
-            number = int(input("Enter an odd number: "))
-            if number % 2 == 0:
+            number = int(input("Enter an odd number: "))  # enter odd number
+            if number % 2 == 0:  # for even number
                 raise EvenNumberError(number)
             else:
                 return number
         except ValueError:
             print("Error: Please enter a valid integer.")
 
-
-# Example usage:
 try:
     odd_number = get_odd_number_from_user()
     print("You entered:", odd_number)
