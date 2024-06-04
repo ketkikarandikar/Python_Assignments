29e2587b2689bc9711bfe601c322755fde823d91
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:27:51 +0530

    Update question--4.py

--- Module_4(Advance python programming)/question--4.py
+++ Module_4(Advance python programming)/question--4.py
@@ -1,16 +1,15 @@
 # Write a Python program to read last n lines of a file. 
 
-def read_last_n_lines(file_path, n):
+def read_last_n_lines(file_path, n): # defination read the last n lines of a file
     try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            last_n_lines = lines[-n:]
+        with open(file_path, 'r') as file:  # open a file
+            lines = file.readlines()  # to read the lines
+            last_n_lines = lines[-n:]  # to go to the end and read lines
             for line in last_n_lines:
-                print(line.rstrip('\n'))
+                print(line.rstrip('\n'))  # to remove the new line character
     except FileNotFoundError:
-        print("File not found!")
+        print("File not found!")  # except the error
 
-# Example usage:
-file_path = "sample.txt"  # Replace "example.txt" with the path to your text file
+file_path = "sample.txt"  
 n = 5  # Number of lines to read from the end
 read_last_n_lines(file_path, n)
