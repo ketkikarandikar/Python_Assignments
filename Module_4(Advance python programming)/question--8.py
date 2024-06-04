d4fcd78385a2595b353b203c81b350ec1288d67e
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:36:03 +0530

    Update question--8.py

--- Module_4(Advance python programming)/question--8.py
+++ Module_4(Advance python programming)/question--8.py
@@ -1,16 +1,15 @@
 # Write a Python program to count the number of lines in a text file.
 
-def count_lines(file_path):
-    line_count = 0
+def count_lines(file_path):  # defination to count the lines in the text file
+    line_count = 0  # start from
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r') as file:  # open the file
             for line in file:
-                line_count += 1
+                line_count += 1  # count the line
     except FileNotFoundError:
         print("File not found!")
     return line_count
 
-# Example usage:
-file_path = "sample.txt"  # Replace "example.txt" with the path to your text file
-number_of_lines = count_lines(file_path)
-print("Number of lines in the file:", number_of_lines)
+file_path = "sample.txt"  
+number_of_lines = count_lines(file_path) # count the number of lines and store in the number_of_lines
+print("Number of lines in the file:", number_of_lines)  # print the number of lines in the text file
