de386fdb4ef1d1112b62f1786c76db51735d8deb
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:23:25 +0530

    Update question--2.py

--- Module_4(Advance python programming)/question--2.py
+++ Module_4(Advance python programming)/question--2.py
@@ -1,23 +1,22 @@
 # Write a Python program to append text to a file and display the text.
 
-def append_to_file(file_path, text_to_append):
+def append_to_file(file_path, text_to_append): # defination to append a sentence in a file
     try:
-        with open(file_path, 'a') as file:
-            file.write(text_to_append)
+        with open(file_path, 'a') as file:  # open a file to append the sentence
+            file.write(text_to_append)  # write in a file to append a sentence
     except FileNotFoundError:
-        print("File not found!")
+        print("File not found!")  # To except an error
 
-def display_file_contents(file_path):
+def display_file_contents(file_path):  # defination to display file contents
     try:
-        with open(file_path, 'r') as file:
-            content = file.read()
+        with open(file_path, 'r') as file:  # open a file
+            content = file.read()  # open a file content
             print(content)
-    except FileNotFoundError:
+    except FileNotFoundError:  # except an error
         print("File not found!")
 
-# Example usage:
-file_path = "sample.txt"  # Replace "example.txt" with the path to your text file
-text_to_append = "\nThis is some additional text to append."
-append_to_file(file_path, text_to_append)
-print("Updated contents:")
-display_file_contents(file_path)
+file_path = "sample.txt"  # file path
+text_to_append = "\nThis is some additional text to append."  # to append a new sentence
+append_to_file(file_path, text_to_append)  
+print("Updated contents:")  # print the new line added to the text file
+display_file_contents(file_path)  # display the content in the file
