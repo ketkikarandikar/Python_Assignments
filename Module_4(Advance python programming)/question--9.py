5acdc97743aba8531f7f85be053b59cd40f83548
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:37:48 +0530

    Update question--9.py

--- Module_4(Advance python programming)/question--9.py
+++ Module_4(Advance python programming)/question--9.py
@@ -1,11 +1,11 @@
 # Write a Python program to count the frequency of words in a file.
 
-def count_word_frequency(file_path):
-    word_frequency = {}
+def count_word_frequency(file_path): # defination to count the word frequency
+    word_frequency = {}  # take an empty set
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r') as file:  # open a file
             for line in file:
-                words = line.split()
+                words = line.split()  # split line into words
                 for word in words:
                     word = word.strip(",.?!")  # Remove punctuation marks
                     word_frequency[word] = word_frequency.get(word, 0) + 1
@@ -13,7 +13,6 @@ def count_word_frequency(file_path):
         print("File not found!")
     return word_frequency
 
-# Example usage:
-file_path = "sample.txt"  # Replace "example.txt" with the path to your text file
+file_path = "sample.txt"  
 word_frequency = count_word_frequency(file_path)
-print("Word frequencies:", word_frequency)
+print("Word frequencies:", word_frequency)   # print the frequency of the words means how many times the words are there in the sentence
