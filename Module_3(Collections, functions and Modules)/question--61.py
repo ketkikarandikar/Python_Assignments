87ee4ba7eaf2fdeb4d6ecf915d7e8bab60343824
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:45:55 +0530

    Update question--61.py

--- Module_3(Collections, functions and Modules)/question--61.py
+++ Module_3(Collections, functions and Modules)/question--61.py
@@ -1,11 +1,11 @@
 # Write a Python program to find the maximum and minimum numbers from the specified decimal numbers.
 
-def find_max_min(decimal_numbers):
-    maximum = max(decimal_numbers)
-    minimum = min(decimal_numbers)
+def find_max_min(decimal_numbers):  # defination to find the maximum and minimum numbers
+    maximum = max(decimal_numbers)  # max numbers
+    minimum = min(decimal_numbers)  # min numbers
     return maximum, minimum
 
-decimal_numbers = [3.14, 2.71, 1.618, 4.669, 0.577]
-maximum, minimum = find_max_min(decimal_numbers)
-print("Maximum number:", maximum)
-print("Minimum number:", minimum)
+decimal_numbers = [3.14, 2.71, 1.618, 4.669, 0.577]  # get a list
+maximum, minimum = find_max_min(decimal_numbers)  # find the min and max
+print("Maximum number:", maximum)  # print the max numbers
+print("Minimum number:", minimum) # print the min numbers
