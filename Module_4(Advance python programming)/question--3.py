15ba8f9348f40bd6f5bf33c04acae003bb22765d
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-06-04 15:25:21 +0530

    Update question--3.py

--- Module_4(Advance python programming)/question--3.py
+++ Module_4(Advance python programming)/question--3.py
@@ -1,18 +1,17 @@
 # Write a Python program to read first n lines of a file. 
 
-def read_first_n_lines(file_path, n):
+def read_first_n_lines(file_path, n):  # defination to read first n lines of a file
     try:
-        with open(file_path, 'r') as file:
-            for i in range(n):
+        with open(file_path, 'r') as file:  # open a file
+            for i in range(n):  # take range till n 
                 line = file.readline().rstrip('\n')  # Read each line and remove the newline character
                 if line:
                     print(line)
                 else:
                     break
     except FileNotFoundError:
-        print("File not found!")
+        print("File not found!")  # except an error 
 
-# Example usage:
-file_path = "sample.txt"  # Replace "example.txt" with the path to your text file
+file_path = "sample.txt" 
 n = 5  # Number of lines to read
 read_first_n_lines(file_path, n)
